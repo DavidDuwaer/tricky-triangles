8df4e86b898019ebc74d36cbfdbddcf674e05c53
D.F. Duwaer <davidduwaer@gmail.com>
2016-01-04 23:34:30 +0100

    Built a bit of the layout.

--- Tricky-Triangles/src/GUI/GUI.java
+++ Tricky-Triangles/src/GUI/GUI.java
@@ -2,32 +2,107 @@ package GUI;
 
 import Graph.Graph;
 import TrickyTriangles.TrickyTriangles;
+import java.awt.Container;
+import java.awt.LayoutManager;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Group;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 /**
  *
  * @author s115869
  */
 public class GUI {
-    
-    Panel panel;
+
+    Panel workCanvas;
+    Panel goalCanvas;
     JFrame frame;
-    
-    public GUI(TrickyTriangles main){
-        panel = new Panel(main);
-        panel.addMouseListener(panel);
-        frame = new JFrame("Geometric algorithms - the triangulation game");
-        frame.add(panel);
-        frame.setVisible(true);
-        frame.setSize(1600,875); 
-        frame.setLocation(0, 0);
+
+    public GUI(TrickyTriangles main) {
+        /*
+         * Window
+         */
+        frame = new JFrame();
+        frame.setTitle("Tricky Triangles");
+        frame.setSize(800, 500);
+        frame.setLocation(20, 20);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //frame.getContentPane().setBackground( Color.BLACK );
+
+        Container pane = frame.getContentPane();
+        GroupLayout gl = new GroupLayout(pane);
+        pane.setLayout(gl);
+
+        gl.setAutoCreateContainerGaps(true);
+
+        /*
+         * Canvas
+         */
+        workCanvas = new Panel(main);
+        goalCanvas = new Panel(main);
+
+        /*
+         * "New game" button
+         */
+        JButton newGame = new JButton("New game");
+
+        gl.setVerticalGroup(gl.createSequentialGroup().addComponent(newGame));
+
+        /*
+         * "Reset game" button
+         */
+        JButton resetGame = new JButton();
+        resetGame.setText("Reset game");
+
+        /*
+         * Make layout
+         */
+//        gl.setHorizontalGroup(
+//                gl.createSequentialGroup()
+//                .addComponent(newGame)
+//                .addComponent(resetGame)
+//        );
+//        gl.setVerticalGroup(
+//                gl.createParallelGroup()
+//                .addComponent(newGame)
+//                .addComponent(resetGame)
+//        );
+        gl.setHorizontalGroup(
+                gl.createParallelGroup()
+                        .addGroup(gl.createSequentialGroup()
+                                .addComponent(workCanvas)
+                                .addComponent(goalCanvas)
+                        )
+                        .addGroup(gl.createSequentialGroup()
+                                .addComponent(newGame)
+                                .addComponent(resetGame)
+                        )
+        );
+        gl.setVerticalGroup(
+                gl.createSequentialGroup()
+                        .addGroup(gl.createParallelGroup()
+                                .addComponent(workCanvas)
+                                .addComponent(goalCanvas)
+                        )
+                        .addGroup(gl.createParallelGroup()
+                                .addComponent(newGame)
+                                .addComponent(resetGame)
+                        )
+        );
+        frame.setVisible(true);
     }
-    
-    public void show(Graph g){
-        panel.set(g);
-        panel.repaint();
+
+    public void show(Graph g) {
+        workCanvas.set(g);
+        workCanvas.repaint();
+        goalCanvas.set(g);
+        goalCanvas.repaint();
     }
-    
+
 }
